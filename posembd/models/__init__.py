bb2a3eb710afe7d6ad618947aeb1ed9b7ff27e5a
pauloamed <pauloaugusto99@ufrn.edu.br>
2019-11-24 14:48:13 -0300

    test

--- posembd/models/__init__.py
+++ posembd/models/__init__.py
@@ -1,4 +1,4 @@
-from .CharBiLSTM import CharBiLSTM
-from .WordBiLSTM import WordBiLSTM
-from .POSTagger import POSTagger
-from .utils import createPOSModel, loadPOSModel, savePOSModel
+from . import CharBiLSTM
+from . import WordBiLSTM
+from . import POSTagger
+from . import createPOSModel, loadPOSModel, savePOSModel
